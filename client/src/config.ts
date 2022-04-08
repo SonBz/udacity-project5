73ebbb2603c238a7d712606ddd924b961aaf243f
sonbz <nguyentason.13021998@gmail.com>
2022-04-08 17:02:56 +0700

    08042022

---
+++ client/src/config.ts
@@ -0,0 +1,10 @@
+
+const apiId = '1vvboweu39'
+export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
+
+export const authConfig = {
+
+  domain: 'dev-87g9hlzt.us.auth0.com',            // Auth0 domain
+  clientId: 'GFkNaxL43ROtIo2EUXi9tuVEOJa49mEJ',          // Auth0 client id
+  callbackUrl: 'http://localhost:3000/callback'
+}
